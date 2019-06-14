8e8800666464b5f0c27b3e2870fbff7424807378
Asep Septiadi <mcreator.id.dev@gmail.com>
2019-06-14 11:41:51 +0700

    RESTful API Laravel #1.2 - Update README.md primary in Project

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,9 +12,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user = new \App\User;
-        $user->name = "Asep Septiadi";
-        $user->email = "asep@rbs.sch.id";
-        $user->password = Hash::make("21091994");
+        $user->name = "Rusydi Faridi";
+        $user->email = "rusydi@rbs.sch.id";
+        $user->password = Hash::make("224423223");
         $user->api_token = str_random(100);
         $user->save();
     }
